bea5bfb948d59f3957a52950d8a708cc4edc3961
carlosagullo <carlos.agullo@gmail.com>
2017-04-11 10:48:55 +0000

    Removing some redundancy. Sticking to charlist instead of strings

--- roman.exs
+++ roman.exs
@@ -9,23 +9,25 @@ defmodule Roman do
     decs = div((number - 1000*thousands - 100*hundreds), 10)
     units = number - 1000*thousands - 100*hundreds - 10*decs
 
-    get_thousands(thousands) <>
-    transform_into(hundreds,"C",%{rep9: "CM", rep5: "D", rep4: "CD"}) <>
-    transform_into(decs,"X",%{rep9: "XC", rep5: "L", rep4: "XL"}) <>
-    transform_into(units,"I",%{rep9: "IX", rep5: "V", rep4: "IV"})
+    get_thousands(thousands) ++
+    transform_into(hundreds,"C",%{rep9: 'CM', rep5: 'D', rep4: 'CD'}) ++ 
+    transform_into(decs,"X",%{rep9: 'XC', rep5: 'L', rep4: 'XL'}) ++ 
+    transform_into(units,"I",%{rep9: 'IX', rep5: 'V', rep4: 'IV'})
+    |> List.to_string
   end
   
-  def get_thousands(thousands), do: String.duplicate("M", thousands)
+  def get_thousands(thousands) do 
+    String.duplicate("M", thousands)
+    |> String.to_char_list
+  end
   
   def transform_into(unit, matching_condition, map_reps) do
     [to_match] = String.to_char_list(matching_condition)
     String.duplicate(matching_condition, unit)
     |> to_charlist
-    |> check_repetitions(9, 0, to_match,map_reps[:rep9],[""])
-    |> to_charlist
-    |> check_repetitions(5, 0, to_match,map_reps[:rep5],[""])
-    |> to_charlist
-    |> check_repetitions(4, 0, to_match,map_reps[:rep4],[""])
+    |> check_repetitions(9, 0, to_match,map_reps[:rep9],[''])
+    |> check_repetitions(5, 0, to_match,map_reps[:rep5],[''])
+    |> check_repetitions(4, 0, to_match,map_reps[:rep4],[''])
   end
   
   # -------------------
@@ -33,9 +35,8 @@ defmodule Roman do
   # -------------------
   # End of list
   def check_repetitions([] , repetitions, repetitions, _, substitute_by, _), do: substitute_by
-  def check_repetitions([] , repetitions, _, _, _, list), do: List.to_string(list)
-  # For partial matches
-  def check_repetitions([h|t] , repetitions, repetitions, _, substitute_by, list), do: substitute_by <> List.to_string([h] ++ t)
+  def check_repetitions([] , _, _, _, _, list), do: list
+  def check_repetitions([h|t] , repetitions, repetitions, _, substitute_by, _), do: substitute_by ++ ([h] ++ t)
   #
   
   # Match on the character I am looking for, "h"
@@ -47,6 +48,5 @@ defmodule Roman do
   def check_repetitions([h|t] , repetitions, acc, search_for, substitute_by, original_list) do
     check_repetitions(t, repetitions, acc, search_for, substitute_by, original_list ++ [h])
   end
-
 
 end
